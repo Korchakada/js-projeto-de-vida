ad5517034443b3c4d401671be93e26fa10b491f6
Korchakada <163421332+Korchakada@users.noreply.github.com>
2025-08-21 00:38:47 -0300

    id

--- main.js
+++ main.js
@@ -36,13 +36,20 @@ function calculaTempo(tempoObjetivo1){
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas +  " horas " + minutos + " minutos " + segundos + " segundos "
+        if (tempoFinal> 0){
+    return [dias, horas, minutos, segundos];
+    }else {
+        return[0, 0, 0, 0];
+    }
 
 }
 
 function atualizarCronometro(){
     for(let i = 0; i < contadores.length; i++)
-        contadores [i].textContent = calculaTempo(tempois[i]);
+        document.getElementById("dias" + i) .textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("dias" + i) .textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("dias" + i) .textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("dias" + i) .textContent = calculaTempo(tempos[i])[3];
 }
 
 function comecaCronometro(){
