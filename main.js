6337687c2d60d4395fcdf39318a42beb5ad3205e
Korchakada <163421332+Korchakada@users.noreply.github.com>
2025-08-21 00:29:32 -0300

    horario

--- main.js
+++ main.js
@@ -13,12 +13,23 @@ for(let i = 0; i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo = new Date("2025-10-05T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[0].textContent = calculaTempo(tempoObjetivo);
 
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
-    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas +  " horas " + minutos + " minutos " + segundos + " segundos "
+
 }
